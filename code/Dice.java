512fa5daddb7bff759e23346b41e02486901d0d2
Joseph Carpenter <carpentj@lawrence.edu>
2023-02-28 09:10:23 -0600

    committing

--- code/Dice.java
+++ code/Dice.java
@@ -1,18 +1,20 @@
+import java.util.Random;    //Importing random class
 public class Dice {
     public int numFaces;
     public Random rand;
 
-    public Dice(faces) {
+    public Dice(int faces) {    //Declared faces as an int
         this.numFaces = faces;
         this.rand = new Random();
     }
 
     public int roll() {
-        return rand.nextInt(this.numFaces);
+        return rand.nextInt(this.numFaces) + 1; //Added one to returned value because rolling zero is impossible, nextInt is exclusive with argument
     }
 
-    public void toString() {
-        System.out.println("d" + this.numFaces)
+    public String toString() {    //Changed method to return String
+        return "d" + this.numFaces;    //Not printing, returning string
+
     }
 
 }
