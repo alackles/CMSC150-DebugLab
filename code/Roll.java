032e56e52300de4eaa43c464a44f872c46736088
cristiantallarico <cristian.tallarico@lawrence.edu>
2023-02-28 13:40:09 -0600

    Debug Roll and Dice class

--- code/Roll.java
+++ code/Roll.java
@@ -3,20 +3,20 @@ import java.util.Arrays;
 public class Roll {
 
    public static void main(String[] args) {
-        int n = args[0];
-        int k = args[1];
+        int n = Integer.parseInt(args[0]); //parseInt from String
+        int k = Integer.parseInt(args[1]); //pareInt from String
         
         Dice dice = new Dice(6);
         
-        int[] rolls;
-        int sum;
+        int[] rolls = new int[k];
+        int sum = 0; //Sum set to 0
 
         for (int i = 0; i < k; i++) {
             rolls[i] = dice.roll();
             sum += dice.roll();
         }
 
-        System.out.println(k + dice.toString() + ":" + sum + Arrays.toString(rolls));
+        System.out.println(k + dice.toStringMethod() + ": " + sum + Arrays.toString(rolls)); //changed to dice.toStringMethod
 
    } 
 }
