5d1d7315cfc473a6dafeaa5e1e4cdcf79111be27
Jude Lawler <lawlerj@lawrence.edu>
2023-03-07 09:22:07 -0600

    Debugged Code in both classes

--- code/Dice.java
+++ code/Dice.java
@@ -1,18 +1,20 @@
+import java.util.Random; // imported Random class for RNG
+
 public class Dice {
     public int numFaces;
-    public Random rand;
+    public Random rand = new Random();  // declared new Random
 
-    public Dice(faces) {
+    public Dice(int faces) {    // added 'int' declaration in parameters
         this.numFaces = faces;
         this.rand = new Random();
     }
 
     public int roll() {
-        return rand.nextInt(this.numFaces);
+        return rand.nextInt(this.numFaces) + 1; // added 1 so that dice cannot roll 0 and can roll the max numFaces
     }
 
-    public void toString() {
-        System.out.println("d" + this.numFaces)
+    public String toString() {  // changed return value from void to String
+        return "d" + numFaces;  // changed from print statement to return; returns String
     }
 
 }
--- code/Roll.java
+++ code/Roll.java
@@ -3,20 +3,21 @@ import java.util.Arrays;
 public class Roll {
 
    public static void main(String[] args) {
-        int n = args[0];
-        int k = args[1];
+        int n = Integer.valueOf(args[0]);   // changed to convert args Strings to Integer values
+        int k = Integer.valueOf(args[1]);
         
-        Dice dice = new Dice(6);
+        Dice dice = new Dice(k);    // changed value of numFaces given to Dice to k variable rather than a constant '6'
         
-        int[] rolls;
-        int sum;
+        int[] rolls = new int[n];   // changed to set rolls array length to input value n
+        int sum = 0;    // set sum to 0 at beginning
 
-        for (int i = 0; i < k; i++) {
-            rolls[i] = dice.roll();
-            sum += dice.roll();
+        for (int i = 0; i < n; i++) {   // changed from i < k to i < n - should be number of dice, not number of faces
+            int roll = dice.roll(); // only rolls once for both rolls[i] and sum - added this int variable to ensure this
+            rolls[i] = roll;
+            sum += roll;
         }
 
-        System.out.println(k + dice.toString() + ":" + sum + Arrays.toString(rolls));
-
+        System.out.println(n + dice.toString() + ": " + sum + " " + Arrays.toString(rolls));
+            // changed first variable printed from k to n, added space after colon, and added space between sum & printed array
    } 
 }
